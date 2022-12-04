844ad52db22e16e67db31270eda9fcd96f68e1e5
S <s>
2022-12-04 23:23:14 +0400

    {"types":"C","runs":9}

--- Hangman.java
+++ Hangman.java
@@ -83,6 +83,8 @@ public class Hangman extends ConsoleProgram {
     		
     		incorrectChar = checkCharType(userChar);
     		
+    		println("hello)");
+    		
     		printingSystem(incorrectChar);
     		
     	}
