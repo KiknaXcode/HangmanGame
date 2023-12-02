95f7e0eef45c723e510700d01f25ada73df88678
S <s>
2023-12-02 20:47:26 +0400

    {"types":"C","runs":4}

--- Hangman.java
+++ Hangman.java
@@ -28,9 +28,12 @@ public class Hangman extends ConsoleProgram {
     	
     	String listWord = takeWordFromList();
     	
-    	playWhileLivesExists(); // give user right to guess numbers while lives exists
+    	String dashWord = convertWordIntoDash(listWord);
+    	
+    	println(dashWord);
+    	
+    	// playWhileLivesExists(); // give user right to guess numbers while lives exists
 
-    	println(gameStartWord());
     	
 
 	}
@@ -50,114 +53,42 @@ public class Hangman extends ConsoleProgram {
     }
     
     
+    // takes String like "COMPUTER" and converts it into dash representation
+    // and then returns it.
+    
+    // for example: "COMPUTER" --> "--------"
+    
+	private String convertWordIntoDash(String listWord){
+		
+		String deshWord = "";
+		
+		for(int i = 0; i < listWord.length(); i++){
+			deshWord = deshWord +  "-";
+		}
+		
+		return deshWord;
+		
+	}	
     
     
-    
+	
+	
     private void playWhileLivesExists(){
     	
     	while(gameOn){
     		
     		
     		
-    		int counter = 0;
-    		
-    		char userChar = usersChar(); // converting string to char and returns it
-    		
-    		
-    		String exitingWord = giveMeNewDesh(userChar); // changes desh --> "-----" => "--S--";
-    		
-    		printingSystem(userChar);
-    		
-    		
-    		println("The word now looks like this: " + exitingWord);
-    		
-    		counter++;
-    		
-    		if(counter == 20){
-    			gameOn = false;
-    		}
-    		
+
     	}
     }
 
-    char userChar;
     
-    private char usersChar() {
-    	
-    	boolean incorrectChar = true;
-    	
-    	while(incorrectChar){
-    		
-    		String userStringChar = readLine("Your guess: "); // getting single letter string
-    		
-    		userStringChar = userStringChar.toUpperCase();
-    		
-    		userChar = userStringChar.charAt(0);
-    		
-    		incorrectChar = checkCharType(userChar);
-    		
-    		
-    	}
-    	
-    	return userChar;
-
-	}
+    
+    
+ 
 
 
-
-	
-	private String giveMeNewDesh(char userChar){
-		
-		String newDeshWord = "";
-		
-		for(int i = 0; i < listWord.length(); i++){
-			
-			if(listWord.charAt(i) == userChar){
-				
-				newDeshWord = newDeshWord +  userChar;
-				
-			}else{
-				
-				newDeshWord = newDeshWord +  "-";
-	    	
-				// String changedDeshWord = changeDeshWord(indexOfChar, userChar); // returning changed version of deshWord
-			}
-			
-		}
-		
-		return newDeshWord;
-		
-		
-		
-	}	
-	
-	private boolean checkCharType(char userChar){  // checks if charachter is letter or not
-		if(Character.isLetter(userChar)){
-			return false;
-		}else{
-			
-			return true;
-		}
-		
-	}
-	
-	
-	private String gameStartWord(){
-		String deshWord = "";
-		
-		for(int i = 0; i < listWord.length(); i++){		
-				deshWord = deshWord +  "-";	
-		}
-		return deshWord;
-	}
-	
-	private void printingSystem(char userChar){
-		
-		println("There are no chosen Letter in the word");
-	    livesPerTry--;
-	    println("You have " + livesPerTry + " guessess left.");
-		
-	}
 		
 		
 
